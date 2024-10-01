8d9c153e5517b02409213f99d6b8b8012ba9730c
mlyddon <mlyddon@agleader.com>
2024-09-30 20:02:30 -0500

    Stubbed in code for PostgreSql. Switching to SQLLite for quick dev purposes

--- src/MedTrackerAPI/Program.cs
+++ src/MedTrackerAPI/Program.cs
@@ -1,9 +1,24 @@
+using MedTrackerAPI.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Testcontainers.PostgreSql;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// TODO - Get App to use environment
+/*if (builder.Environment.IsDevelopment())
+{
+    await PostgreSqlContainerFactory.CreateAsync();
+}*/
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+/*builder.Services.AddDbContext<MedTrackerDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("MedTracker"));
+});*/
 
 var app = builder.Build();
 
@@ -17,3 +32,26 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.Run();
+
+//TODO Implement PosgresSql. Need to get docker running
+public sealed class PostgreSqlContainerFactory : IAsyncDisposable
+{
+    private static readonly List<PostgreSqlContainer> PostgreSqlContainers = [];
+
+    private PostgreSqlContainerFactory() { }
+
+    public async ValueTask DisposeAsync() => await ValueTask.FromResult(PostgreSqlContainers.Select(c => c.DisposeAsync()));
+
+    public static async Task<string> CreateAsync()
+    {
+        var postgreSqlContainer = new PostgreSqlBuilder()
+            .WithImage("postgres:latest")
+            .Build();
+
+        await postgreSqlContainer.StartAsync();
+
+        PostgreSqlContainers.Add(postgreSqlContainer);
+
+        return postgreSqlContainer.GetConnectionString();
+    }
+}
