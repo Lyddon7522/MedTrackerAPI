1f5bb63cd3a4394d648d14fd9333821c246e123e
mlyddon <mlyddon@agleader.com>
2024-10-06 19:56:49 -0500

    Added Endpoint and Validation Rules

--- src/MedTrackerAPI/Features/Supplies/AddSupply.cs
+++ src/MedTrackerAPI/Features/Supplies/AddSupply.cs
@@ -1,5 +1,6 @@
 using FluentValidation;
 using MediatR;
+using MedTrackerAPI.Endpoints;
 using MedTrackerAPI.Infrastructure;
 
 namespace MedTrackerAPI.Features.Supplies;
@@ -8,9 +9,33 @@ public static class AddSupply
 {
     public record Response (int SupplyId, string? Description, string? Manufacturer, string? PartNumber, string? LotNumber, int DeviceId);
     
+    public class AddSupplyCommandValidator : AbstractValidator<AddSupplyCommand>
+    {
+        public AddSupplyCommandValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required.");
+            RuleFor(x => x.PartNumber).NotEmpty().WithMessage("Part number is required.");
+            RuleFor(x => x.LotNumber).NotEmpty().WithMessage("Lot number is required.");
+            RuleFor(x => x.DeviceId).NotEmpty().WithMessage("Device Id is required.");
+        }
+    }
+
+    public sealed class Endpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPost("supplies", async (IMediator mediator, AddSupplyCommand command) =>
+            {
+                var supply = await mediator.Send(command);
+
+                return Results.Created("", supply);
+            }).WithTags("Supplies")
+            .AddEndpointFilter<ValidationFilter<AddSupplyCommand>>();
+        }
+    }
+    
     public class AddSupplyCommand : IRequest<Response>
     {
-        public int Id { get; set; }
         public required string Description { get; set; }
         public string? Manufacturer { get; set; }
         public required string PartNumber { get; set; }
