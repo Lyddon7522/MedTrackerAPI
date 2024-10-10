868a17dec8a8f03cc31e05027e9f5893fe3ad867
mlyddon <mlyddon@agleader.com>
2024-10-10 14:54:47 -0500

    Implemented Registration Service to register user with Firebase

---
+++ src/MedTrackerAPI/Authentication/AuthenticationService.cs
@@ -0,0 +1,19 @@
+using FirebaseAdmin.Auth;
+
+namespace MedTrackerAPI.Authentication;
+
+internal sealed class AuthenticationService : IAuthenticationService
+{
+    public async Task<string> RegisterUserAsync(string email, string password)
+    {
+        var userArgs = new UserRecordArgs
+        {
+            Email = email,
+            Password = password
+        };
+        
+        var userRecord = await FirebaseAuth.DefaultInstance.CreateUserAsync(userArgs);
+
+        return userRecord.Uid;
+    }
+}
