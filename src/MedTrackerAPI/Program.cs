39049f750a71e4c4eb79df2a6c79066ea23c82c3
mlyddon <lyddon.morg@gmail.com>
2024-10-14 15:30:13 -0500

    Added Auth to Swagger

--- src/MedTrackerAPI/Program.cs
+++ src/MedTrackerAPI/Program.cs
@@ -3,6 +3,7 @@ using MedTrackerAPI.Endpoints;
 using MedTrackerAPI.Infrastructure;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +16,36 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "MedTracker API", Version = "v1"
+    });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please provide a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
@@ -48,8 +78,6 @@ builder.Services.AddAuthentication().AddJwtBearer(JwtBearerDefaults.Authenticati
     jwtOptions.Authority = builder.Configuration["Authentication:Issuer"];
     jwtOptions.Audience = builder.Configuration["Authentication:Audience"];
     jwtOptions.TokenValidationParameters.ValidIssuer = builder.Configuration["Authentication:Issuer"];
-    //Todo: Remove this for production
-    jwtOptions.RequireHttpsMetadata = false;
 });
 builder.Services.AddAuthorization();
 
